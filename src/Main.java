9282a36d9c1538a587209bf89e540f4db1baf6ca
Mimis Chlympatsos <mimis.chlympatsos@mail.utoronto.ca>
2023-09-13 00:51:11 -0400

    creating new Main class and changing access modifier of FizzBuzz.doFizzBuzz

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] someStringArray = {"required string"};
+
+        FizzBuzz.doFizzBuzz(3);
+
+        FizzBuzz.main(someStringArray);
+    }
+}
